240a7cbbf2f2a75003cf44d848ef3c64c9ce552f
Lucas Ewing <lucas.ewing2000@gmail.com>
2022-02-20 20:41:06 -0800

    fin

--- sql/12.sql
+++ sql/12.sql
@@ -6,3 +6,21 @@
  * Create a select statement that lists the titles of all tables with the 'Trailers' special_feature.
  * Inner join the queries above.
  */
+select x.title
+from (
+    select title
+    from (
+        select title, unnest(special_features) as special_feature
+        from film
+    ) t
+    where special_feature='Behind the Scenes'
+) x
+join (
+    select title
+    from (
+        select title, unnest(special_features) as special_feature
+        from film
+    ) z
+    where special_feature='Trailers'
+) s on (x.title=s.title)
+order by x.title;
